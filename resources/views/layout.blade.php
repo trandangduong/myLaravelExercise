9c804e456b38f4cdd2da55cba32fe29e7136c713
trandangduong <duongtran.bus@gmail.com>
2020-09-15 10:38:43 +0700

    ok

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,17 +10,40 @@
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="customers">Customers</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about">About Us</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
 <div class="container">
-
-    @include('nav')
-
-    @if(session()->has('message'))
-        <div class="alert alert-success" role="alert">
-            <strong>Success</strong> {{ session()->get('message') }}
-        </div>
-    @endif
-
-    @yield('content')
+    @yield('content');
 </div>
 
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome');
-Route::view('contact-us', 'contact');
+Route::view('contact', 'contact');
 Route::view('about', 'about');
 
 Route::get('customers',function(){
