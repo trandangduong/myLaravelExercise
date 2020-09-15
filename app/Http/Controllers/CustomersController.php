b43215e9febb346349e29c6df57dde93ae9e8967
trandangduong <duongtran.bus@gmail.com>
2020-09-15 10:57:04 +0700

    ok

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
-    //
+    public function list()
+    {
+        $customers = [
+            'Nguyen Van A',
+            'Tran Van B',
+            'C',
+        ];
+        return view ('internals.customers', [
+            'customers' => $customers,
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controller\CustomersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,21 +17,5 @@ Route::view('/', 'welcome');
 Route::view('contact', 'contact');
 Route::view('about', 'about');
 
-Route::get('customers',function(){
-    $customers = [
-        'Nguyen Van A',
-        'Tran Van B',
-        'C',
-    ];
-    return view ('internals.customers', [
-        'customers' => $customers,
-    ]);
-});
- 
-/* Route::get('contact', function () {
-    return view('contact');
-});
-Route::get('about', function () {
-    return view('about');
-}); */
+Route::get('customers', [CustomersController::class, 'list']);
 
