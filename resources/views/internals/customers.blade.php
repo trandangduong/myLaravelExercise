2b7a9225d846e5ebaa32a5e63ee33c06756cb1f5
trandangduong <duongtran.bus@gmail.com>
2020-09-16 14:34:19 +0700

    16-9

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -3,9 +3,29 @@
 @section('content')
     <h1>Customers</h1>
     
+    <form action="customers" method="POST" class="pb-5">
+        <p>Name:</p>
+        <div class="input-group pb-2">
+            <input type="text" name="name">    
+            <div>{{$errors->first('name') }}</div>
+        </div>
+        
+        <p>Email:</p>
+        <div class="input-group pb-3">
+            <input type="text" name="email">    
+            <div>{{$errors->first('email') }}</div>
+        </div>
+
+        <button type="submit">Add Customer</button>
+        
+        <!-- avoid cross-site request forgery attack -->
+        @csrf
+        
+    </form>
+
     <ul>
         @foreach($customers as $customer)
-            <li>{{$customer->name}}</li>
+            <li>{{$customer->name}} <span class="text-muted"></span> {{$customer->name}}</li>
         @endforeach
     </ul>
 @endsection
