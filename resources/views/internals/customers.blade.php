683782c40477b87c1f69a6fc2268282c422ba394
trandangduong <duongtran.bus@gmail.com>
2020-09-18 10:49:05 +0700

    ok

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -14,18 +14,26 @@
     <div class="row">
         <div class="col-12">
             <form action="customers" method="POST" class="pb-5">
-            <p>Name:</p>
-            <div class="input-group pb-2">
+            <div class="form-group">
+                <label for="name">Name</label>
                 <input type="text" name="name">    
                 <div>{{$errors->first('name') }}</div>
             </div>
             
-            <p>Email:</p>
-            <div class="input-group pb-3">
+            <div class="form-group">
+                <label for="email">Email</label>
                 <input type="text" name="email">    
                 <div>{{$errors->first('email') }}</div>
             </div>
 
+            <div class="form-group">
+                <label for="active">Status</label>
+                <select name="active" id="active" class="form-control">
+                    <option value="" disabled>Select Customer status</option>
+                    <option value="1" Active></option>
+                    <option value="0" inactive></option>
+                </select>
+            </div>
             <button type="submit">Add Customer</button>
             
             <!-- avoid cross-site request forgery attack -->
@@ -36,13 +44,25 @@
     </div>
 
     <div class="row">
-        <div class="col-12">
+        <div class="col-6">
+            <h3>Active Customers</h3>
             <ul>
-            @foreach($customers as $customer)
-                <li>{{$customer->name}} <span class="text-primary">{{$customer->email}}</span></li>
+            @foreach($activeCustomers as $activeCustomer)
+                <li>{{$activeCustomer->name}} <span class="text-primary">{{$activeCustomer->email}}</span></li>
             @endforeach
             </ul>
         </div>
     </div>
-    
+
+    <div class="row">
+        <div class="col-6">
+            <h3>Inactive Customers</h3>
+            <ul>
+            @foreach($inactiveCustomers as $inactiveCustomer)
+                <li>{{$inactiveCustomer->name}} <span class="text-primary">{{$inactiveCustomer->email}}</span></li>
+            @endforeach
+            </ul>
+        </div>
+    </div>
+
 @endsection
